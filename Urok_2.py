f4c58c468129d128d61a83856c1021a144358a98
Anton <aymaslov@mail.ru>
2021-09-02 14:49:25 +0500

    Zadacha 2 urok

---
+++ Urok_2.py
@@ -0,0 +1,29 @@
+#primer_1 = 15 * 3
+#print(type(primer_1))
+#print(primer_1)
+
+#primer_2 = 15 / 3
+#print(type(primer_2))
+#print(primer_2)
+
+#primer_3 = 15 // 2
+#print(type(primer_3))
+#print(primer_3)
+
+#primer_4 = 15 ** 2
+#print(type(primer_4))
+#print(primer_4)
+
+#msg = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+#message = ' '.join(msg)
+#message = message[:2] + '"05"' + message[3:]
+#message = message[:47] + '"+05"' + message[49:]
+#message = message[:13] + '"17"' + message[15:]
+#message = message.capitalize()
+#print(message)
+
+employees = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+player = ' '.join(employees)
+player = player.capitalize()
+
+print(player)
